cead17f5ad13a759ec6ee2280008f405ac2e3928
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-04-11 10:11:53 +0200

    Create update.py

---
+++ S_B_FrameWork/update.py
@@ -0,0 +1,13 @@
+import requests, bs4, json
+
+class Version:
+    version = "0.9"
+    typeversion = "[ BETA ]"
+
+def main():
+    send_request = requests.get('https://raw.githubusercontent.com/Sos1ska/Spider_Breaking_FrameWork/sos-utils/S_B_FrameWork/version.json')
+    soup = bs4.BeautifulSoup(send_request.text, 'html.parser').text
+    site_json = json.loads(soup)
+    Handler = site_json
+    if int(Version.version) == int('%s' % (Hanlder["versionInt"]):
+        print('[ root ] - [ INFO 
