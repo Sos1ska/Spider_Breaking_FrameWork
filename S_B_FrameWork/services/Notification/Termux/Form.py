a8642e47b76678e4ad4af464edeb96b95c089e31
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-06-05 10:51:04 +0300

    Delete S_B_FrameWork directory

--- S_B_FrameWork/services/Notification/Termux/Form.py
+++
@@ -1,9 +0,0 @@
-# Author: Sos1ska
-# Link GitHub -> https://github/Sos1ska
-# Link VK -> https://vk.com/nikitasos1ska
-def FORM_CREATE_SH_TOAST(msg):
-    Create_Form = f'termux-toast \'{msg}\''
-    return Create_Form
-def FORM_CREATE_SH_NOTIFICATION(msg):
-    Create_Form = f'termux-notification --action \'{msg}\''
-    return Create_Form
